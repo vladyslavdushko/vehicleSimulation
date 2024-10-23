94233c3ed58df92a10138f4ea496db8ce9408d4c
Vladyslav Dushko <jafter@ukr.net>
2024-10-23 23:44:14 +0300

    publish update

--- obj/Release/net8.0/VehicleSimulationApp.AssemblyInfo.cs
+++ obj/Release/net8.0/VehicleSimulationApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehicleSimulationApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62b632ae3e8c1c059d634a07ca2b747c3d90d97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9329601f20fc01358d1889b02251883334b8ba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehicleSimulationApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehicleSimulationApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
